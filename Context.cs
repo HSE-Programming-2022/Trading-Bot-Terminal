4f0bf43dfdd7a7f103196289a1e17d481c55a704
Amedyan Edward <105710885+Dertemon@users.noreply.github.com>
2022-06-26 17:41:07 +0300

    Add files via upload
    Создание таблицы

---
+++ Context.cs
@@ -0,0 +1,20 @@
+﻿using CryptoTerminal.Core.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CryptoTerminal.Core
+{
+    public class Context : DbContext
+    {
+        public DbSet<User> Users { get; set; }
+
+        public Context() : base("DBConnection")
+        {
+            
+        }
+    }
+}
